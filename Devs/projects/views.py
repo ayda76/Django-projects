ab8f2956a3569c16cf3a2038618a6818e95fa298
ayda76 <aydavahedfrough@yahoo.com>
2023-07-15 10:28:02 +0330

    postgresql set up

--- Devs/projects/views.py
+++ Devs/projects/views.py
@@ -41,7 +41,7 @@ def createProject(request):
     profile=request.user.profile
     form=ProjectForm()
     if request.method == 'POST':
-        newtags=request.POST.get('newtags').replace(','," ",'-').split()
+        #newtags=request.POST.get('newtags').replace(',').split()
         
         form=ProjectForm(request.POST, request.FILES)
         if form.is_valid():
@@ -49,9 +49,9 @@ def createProject(request):
             #we need to asign the project to the profile that created it 
             project.owner=profile
             project.save()
-            for tag in newtags:
-                tag, created=Tag.objects.get_or_create(name=tag)
-                project.tags.add(tag) 
+            #for tag in newtags:
+                #tag, created=Tag.objects.get_or_create(name=tag)
+                #project.tags.add(tag) 
                 
             return redirect('projects')
     context={'form':form}
@@ -64,13 +64,13 @@ def updateProject(request,pk):
     form=ProjectForm(instance=project)
 
     if request.method =='POST':
-        newtags=request.POST.get('newtags').replace(','," ",'-').split()
+        #newtags=request.POST.get('newtags').replace(','," ",'-').split()
         form=ProjectForm(request.POST,request.FILES,instance=project)
         if form.is_valid():
             project=form.save()
-            for tag in newtags:
-                tag,created=Tag.objects.get_or_create(name=tag)
-                project.tags.add(tag)
+            #for tag in newtags:
+                #tag,created=Tag.objects.get_or_create(name=tag)
+                #project.tags.add(tag)
             
             return redirect('account')
 
