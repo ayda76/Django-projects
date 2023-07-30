5aa68bde1c7fa21dbec44de5b329d4a2cbfa1803
ayda76 <aydavahedfrough@yahoo.com>
2023-07-30 19:15:23 +0330

    user authorization added

---
+++ E-Commerce/base/views/order_views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from django.http import JsonResponse
+from base.models import Product
+from django.contrib.auth.models import User
+from base.serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
+
+
+
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from django.contrib.auth.hashers import make_password
+from rest_framework import status
