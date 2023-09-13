07f2e7d436fd1393a60ecfb6af78d2d3fc1ddeb5
ayda76 <aydavahedfrough@yahoo.com>
2023-09-13 20:11:50 +0330

    CRUD oprations for orders is done

--- Resturant_Page/resturant_page/orders/urls.py
+++ Resturant_Page/resturant_page/orders/urls.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.showOrderItems, name='orders')
-  
+    path('create-order/<str:pk>',views.createOrders, name='create-order'),
+    path('delete-order/<str:pk>',views.deleteOrderItem, name='delete-order'),
+    path('update-order/<str:pk>',views.updateOrderItem, name='update-order'),
+    path('',views.showOrderItems, name='orders'),
     
+    path('make-order/',views.makeOrder, name='make-order'),
+    
+  
 ]
--- Resturant_Page/resturant_page/orders/views.py
+++ Resturant_Page/resturant_page/orders/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render,redirect
 from .models import Order, OrderItem
+from foods.models import Food
 from users.models import Profile
 
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 # Create your views here.
 
+@login_required(login_url='login')
 def showOrderItems(request):
     user=request.user
     profile=Profile.objects.get(user=user)
@@ -20,3 +22,90 @@ def showOrderItems(request):
     
     context={'orderItems':orderItems}
     return render(request,'orders/cart.html',context)
+
+@login_required(login_url='login')
+def createOrders(request,pk):
+    food_item=Food.objects.get(id=pk)
+    user=request.user
+    profile=Profile.objects.get(user=user)
+    order=profile.order_set.filter(isPaid=False)
+
+    if order is not None:
+        orderItem=OrderItem.objects.create(
+            order=order,
+            food_item=food_item)
+        orderItem.save()
+        order.save()
+        return redirect('orders')       
+    
+    else:
+        order=Order.objects.create(profile=profile)
+            
+        orderItem=OrderItem.objects.create(
+            order=order,
+            food_item=food_item)    
+                
+        orderItem.save()
+        order.save()
+        return redirect('orders')    
+
+
+    orderItems=order.orderitem_set.all()  
+    context={'orderItems':orderItems} 
+    return render(request,'orders/cart.html',context)       
+
+@login_required(login_url='login')
+def deleteOrderItem(request,pk):
+    orderItem=OrderItem.objects.get(id=pk)
+    orderItem.delete()
+    return redirect('orders')   
+
+@login_required(login_url='login')
+def updateOrderItem(request,pk):
+    orderItem=OrderItem.objects.get(id=pk)
+    qty=request.POST['qty']
+
+    
+    if int(qty) != orderItem.qty & qty is not None:
+        orderItem.qty=int(qty)
+        orderItem.save()
+        return redirect('orders')
+
+    return redirect('orders')
+
+@login_required(login_url='login')
+def makeOrder(request):
+    user=request.user
+    profile=Profile.objects.get(user=user)
+    order=profile.order_set.filter(isPaid=False)
+    
+    context={'order':order,'profile':profile}
+
+    if order is not None:
+        if profile.address is not None:
+            orderItems=order.orderitem_set.all()  
+            
+            context={'orderItems':orderItems,'profile':profile}
+            
+        else:
+            return redirect('update_user')
+
+    
+        
+    return render(request,'orders/paybill.html',context)
+
+        
+        
+
+
+
+            
+        
+
+        
+
+
+        
+
+
+    
